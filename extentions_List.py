0b3f375c88713bb6a348bf71608343ee1ae24f24
R <rulas164@gmail.com>
2016-12-07 12:30:30 -0600

    sooething

---
+++ extentions_List.py
@@ -0,0 +1,2 @@
+extentions_List = ['jpg', 'png', 'gif', 'bmp', 'doc', 'docx', 'xls', 'xlsx', 'ppt', 'pptx', 'txt', 'pdf', 'wav', 'mp3', 'avi', 'wmv', 'rm', 'mp4', 'mpg',\
+    'mov', 'qt', 'swf', 'dll', 'exe', 'zip', 'msi', 'reg', 'bat', 'cmd', 'vbs', 'js', 'jar', 'iso']
